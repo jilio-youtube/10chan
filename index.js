024d234ce475ea4556b8832e34e68e4b42b25efd
jilio-youtube <jiliochannel@gmail.com>
2016-12-24 01:34:15 +0700

    koa server

---
+++ index.js
@@ -0,0 +1,11 @@
+const koa = require('koa');
+const logger = require('koa-logger');
+const app = koa();
+
+app.use(logger());
+
+app.use(function *(){
+  this.body = '<b>10chan</b><br\>Добро пожаловать. Снова.';
+});
+
+app.listen(3000);
